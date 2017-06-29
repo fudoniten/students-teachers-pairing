04cc5d64f7078e08bfabe2c2671236a719bbfc55
Omar Mefire <omefire@gmail.com>
2017-06-28 19:51:34 -0700

    Initial commit

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,37 @@
 module Main where
 
-import Lib
+import Domain
+import Data.Map (Map)
+
+generatePairings :: [(Tutor, Student)] -- State Map [(Tutor, Student)]
+generatePairings = (flip map) getStudents $ \student ->
+  
+
+  -- ToDO: Clarify comment
+  -- Find the 'most optimal tutor' for each subject the student is interested in
+  -- We'll strive to make sure that tutors have approximately the same load
+  let res = (flip map) (getStudentPreferences student) $ \subject ->
+        let tutors = getTutorsForSubject subject
+            appropriateTutor =  getAppropriateTutor tutors student subject
+        in (appropriateTutor, subject)
+  in catMaybes res
+
+
+ -- ToDO: Remove duplicate pairings
+ -- ToDO: Handle case when no teacher can handle a subject desired/preferred by the student
+
+
+-- ToDO: Change this
+getAppropriateTutor :: [Tutor] -> Student -> Subject -> Maybe Tutor
+getAppropriateTutor [] _ _ = None
+getAppropriateTutor tutors _ subject = tutors !! 0
+
+displayPairings :: [(Tutor, Student)] -> IO()
+displayPairings @pairings = do
+  forM pairings $ \ p ->
+    putStrLn "(" ++ fst p ++ ")"
 
 main :: IO ()
-main = someFunc
+main = do
+  let pairings = runState generatePairings $ Map.fromList()
+  displayPairings pairings
